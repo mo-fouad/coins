c58d823a51749ee01071a747f25fb53d14a1a596
mfo <mfo@ordbogen.com>
2019-01-09 16:54:33 +0200

    updating

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import Tickers from './components/Tickers'
+const Header = styled.div`
+  background: #eee;
+  text-align:center;
+  font-family:Arial, Helvetica, sans-serif;
+  font-size:18px;
+  padding: 20px;
+`
+ 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header  className="App-header">
+          <h2>Cryptocurrency converter 
+          </h2>
+        </Header>
+        <Tickers></Tickers>
       </div>
     );
   }
---
+++ src/components/Tickers.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import Cryptocurrency from './Cryptocurrency';
+import axios from 'axios';
+
+const TickerCont = styled.div`
+     
+`;
+
+const Tickeritems = styled.div`
+     
+`;
+
+
+export default class Tickers extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            data:[ 
+                {
+                    id: "bitcoin",
+                    name: "Bitcoin",
+                    symbol: "BTC",
+                    price_usd: "1",
+                    percent_change_1h: "0",
+                    percent_change_24h: "0",
+                    percent_change_7d: "0",
+                },
+                {
+                    id: "ethereum",
+                    name: "Ethereum",
+                    symbol: "ETH",
+                    price_usd: "1",
+                    percent_change_1h: "0",
+                    percent_change_24h: "0",
+                    percent_change_7d: "0",
+                },
+                {
+                    id: "litecoin",
+                    name: "Litecoin",
+                    symbol: "LTC",
+                    price_usd: "1",
+                    percent_change_1h: "0",
+                    percent_change_24h: "0",
+                    percent_change_7d: "0",
+                }
+            ]
+        }
+    }
+
+
+    // realoadin gdata after 60 sec
+    componentDidMount() {
+        this.fetchCryptocurrencyData();
+        this.interval = setInterval(() => this.fetchCryptocurrencyData(), 60 * 1000);
+    }
+
+    // Getting data from the API,
+    fetchCryptocurrencyData() {
+        axios.get("https://api.coinmarketcap.com/v1/ticker/")
+            .then(response => {
+                var wanted = ["bitcoin", "ethereum", "litecoin"];
+                var result = response.data.filter(currency => wanted.includes(currency.id));
+                this.setState({ data: result});
+            })
+            .catch(err => console.log(err));
+    }
+
+
+
+  render() {
+      let tickers = this.state.data.map((coine) => 
+            <Cryptocurrency data= {coine}  key={coine.id}></Cryptocurrency>      
+      ) 
+    return (
+      <TickerCont> 
+        <Tickeritems>
+            {tickers}
+        </Tickeritems>
+      </TickerCont>
+    )
+  }
+}
