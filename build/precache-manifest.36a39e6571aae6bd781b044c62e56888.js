a5427a3c232b9065c385b409b46a5935dcb08b9d
mfo <mfo@ordbogen.com>
2019-01-10 14:43:40 +0200

    trying to build

---
+++ build/precache-manifest.36a39e6571aae6bd781b044c62e56888.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4721eaa5b4002497506b",
+    "url": "/coins/static/js/runtime~main.4721eaa5.js"
+  },
+  {
+    "revision": "ee28bf71978cd793add0",
+    "url": "/coins/static/js/main.ee28bf71.chunk.js"
+  },
+  {
+    "revision": "d6f040fb3967bb9a2550",
+    "url": "/coins/static/js/1.d6f040fb.chunk.js"
+  },
+  {
+    "revision": "0de8de7b94f35b61db19ec6a943af7b7",
+    "url": "/coins/index.html"
+  }
+];
